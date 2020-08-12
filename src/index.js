a629a960d88a9ea63b01cfe87e2e5ae7dafa0e52
Marcus Aschan <marcusaschan@gmail.com>
2020-08-12 14:51:00 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const mix = require('laravel-mix')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+
+class WebApp {
+
+    /**
+     * Dependencies for plugin.
+     *
+     * @return {String[]}
+     */
+    dependencies() {
+        return [
+            'html-webpack-plugin',
+            'favicons-webpack-plugin'
+        ]
+    }
+
+    /**
+     * Plugin functionality.
+     *
+     * @param {logo: String, filename: String, outputPath: String, prefix: String, favicons: Object} options
+     */
+    register(options = {}) {
+        options.logo = path.resolve(options.logo)
+
+        this.options = Object.assign({
+            filename: 'webapp.html',
+            outputPath: '/webicons',
+            prefix: 'webicons/',
+            favicons: {},
+        }, options)
+    }
+
+    /**
+     * @return {Object}
+     */
+    webpackPlugins() {
+        return [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'template.ejs'),
+                filename: this.options.filename,
+                inject: false,
+            }),
+            new FaviconsWebpackPlugin({
+                logo: this.options.logo,
+                outputPath: this.options.outputPath,
+                prefix: this.options.prefix,
+                cache: false,
+                inject: true,
+                favicons: this.options.favicons,
+            }),
+        ]
+    }
+
+    /**
+     *
+     */
+    webpackConfig(webpackConfig) {
+        Mix.manifest.add(this.options.filename)
+    }
+}
+
+mix.extend('webApp', new WebApp())
