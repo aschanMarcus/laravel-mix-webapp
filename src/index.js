736167dcdb4300d8c6043635d0ff706b9b4f5e44
Marcus Aschan <marcus@autovex.fi>
2021-04-21 13:32:37 +0300

    update dependencies, require path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const path = require('path')
 
 class WebApp {
 
